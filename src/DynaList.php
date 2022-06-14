28c2cba1c54a569d2638b5f879c3c41cd7a3e388
midhun <midhun.prakash@digitalmesh.com>
2022-06-14 14:01:21 +0530

    widget tool changes

--- src/DynaList.php
+++ src/DynaList.php
@@ -442,26 +442,57 @@ class DynaList
             $selected = ($eachSize ==  $page->page_size) ? "selected" : "";
             $sizeOptions .= "<option value='{$eachSize}' {$selected}>{$eachSize}</option>";
         }
-        
+        $random = rand (10000, 99999);
         $html = "<div class='custom-pagination'>
-					<a href='javascript:void(0)' class='first-page enabled' title='First' data-page='1'>
+					<a href='javascript:void(0)' class='first-page enabled' title='First' data-page='1' onclick='pagination{$random}(1,this,{$page->page_size},{$page->total_records})'>
       					<span class='glyphicon glyphicon-step-backward'></span>
     				</a>
-    				<a href='javascript:void(0)' class='prev-page enabled' title='Previous' data-page='{$page->prev_page}'>
+    				<a href='javascript:void(0)' class='prev-page enabled' title='Previous' data-page='{$page->prev_page}' onclick='pagination{$random}({$page->prev_page},this,{$page->page_size},{$page->total_records})'>
       					<span class='glyphicon glyphicon-backward'></span>
     				</a>
     				<div class='pagedisplay'>
       					Records {$min} to {$max} (Total {$page->total_records} Results) - Page {$page->page} of {$page->total_pages}
     				</div>
-    				<a href='javascript:void(0)' class='next-page enabled' title='Next' data-page='{$page->next_page}'>
+    				<a href='javascript:void(0)' class='next-page enabled' title='Next' data-page='{$page->next_page}' onclick='pagination{$random}({$page->next_page},this,{$page->page_size},{$page->total_records})'>
       					<span class='glyphicon glyphicon-forward'></span>
     				</a>
-    				<a href='javascript:void(0)' class='last-page enabled' title='Last' data-page='{$page->last_page}'>
+    				<a href='javascript:void(0)' class='last-page enabled' title='Last' data-page='{$page->last_page}' onclick='pagination{$random}({$page->last_page},this,{$page->page_size},{$page->total_records})'>
       					<span class='glyphicon glyphicon-step-forward'></span>
     				</a>
-					<select class='page-limit'>{$sizeOptions}</select>
-  				</div>";
-
+					<select class='page-limit' onchange=paginationBySize{$random}({$page->page},this,{$page->total_records})>{$sizeOptions}</select>
+  				</div>
+                <script>
+                    function pagination{$random}(page, element, page_size, total_records){
+                        //var form_id = element.closest('form').id;
+                        form_id = 'address';
+                        updateHiddenAttribute{$random}('page', page, form_id);
+                        updateHiddenAttribute{$random}('page_size', page_size, form_id);
+                        updateHiddenAttribute{$random}('total_records', total_records, form_id);
+                        document.getElementById(form_id).submit();
+                    }
+                    function paginationBySize{$random}(page, element,total_records){
+                        //var form_id = element.closest('form').id;
+                        form_id = 'address';
+                        var page_size = element.value;
+                        updateHiddenAttribute{$random}('page', page, form_id);
+                        updateHiddenAttribute{$random}('page_size', page_size, form_id);
+                        updateHiddenAttribute{$random}('total_records', total_records, form_id);
+                        document.getElementById(form_id).submit();
+                    }
+                    function updateHiddenAttribute{$random}(name, value, form){
+                        if(document.getElementById(form).elements[name]){
+                            document.getElementById(form_id).elements[name].value = value;
+                        }else{
+                            //addHiddenField{$random}(name, value, form);
+                            var input = document.createElement('input');
+                            input.setAttribute('type', 'hidden');
+                            input.setAttribute('name', name);
+                            input.setAttribute('value', value);
+                            //append to form element that you want .
+                            document.getElementById(form).appendChild(input);
+                        }
+                    }
+                </script>";
         echo $html;
     }
     
